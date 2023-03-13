804f01ae399ae4ce7c6aa1a4ff4afe837db5ebc2
konjikun <shintaniallen@gmail.com>
2023-03-13 22:35:31 +0900

    api error edit

--- server/api/allen/gaming/index.ts
+++ server/api/allen/gaming/index.ts
@@ -1,17 +1,12 @@
-import type { MouseBtn } from '@prisma/client'
-import type { ClickHistoryModel } from 'allen/domain/gaming/model/ClickHistoryModel'
-import type { Pos } from 'allen/domain/gaming/valueObject/Pos'
+import type { ClickHistoryModel, MouseBtn } from '$/allen/domain/gaming/model/ClickHistoryModel'
+import type { Pos } from '$/allen/domain/gaming/valueObject/Pos'
 
 export type Methods = {
   get: {
     resBody: string
   }
   post: {
-    reqBody: {
-      userId: string
-      mouseBtn: MouseBtn
-      pos: Pos
-    }
+    reqBody: { userId: string; mouseBtn: MouseBtn; pos: Pos }
     resBody: ClickHistoryModel[]
   }
 }
