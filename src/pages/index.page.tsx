a5dc2ee0c8bd264d9eb7f5a443102463ef6ac3e9
Yoshiki Higuchi <s1f102201469@iniad.org>
2023-03-14 19:56:06 +0900

    feat: グリッドを仮実装

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -3,7 +3,9 @@ import { useAtom } from 'jotai'
 import { BasicHeader } from 'src/components/organisms/BasicHeader'
 import { DemoModal } from 'src/pages/@components/modal'
 import { userAtom } from '../atoms/user'
+import { DemoGrid } from './@components/grid'
 import { RankDrawer } from './@components/RankDrawer'
+
 const Home = () => {
   const [user] = useAtom(userAtom)
 
@@ -14,6 +16,7 @@ const Home = () => {
         <Space h={48} />
         <DemoModal />
         <RankDrawer />
+        <DemoGrid />
       </>
     )
   )
