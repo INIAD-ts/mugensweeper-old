9dc8c695c9a8ad6ed46da908865474272079122a
Yoshiki Higuchi <s1f102201469@iniad.org>
2023-03-16 19:50:30 +0900

    feat: 拡大・縮小機能を仮実装

--- src/pages/@components/DemoGrid.tsx
+++ src/pages/@components/DemoGrid.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable max-depth */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Layer, Rect, Stage } from 'react-konva'
-
-const WIDTH = 100
-const HEIGHT = 100
+const WIDTH = 30
+const HEIGHT = 30
 
 const grid = [
-  ['red', 'yellow'],
+  ['gray', 'white'],
   ['green', 'blue'],
 ]
 
@@ -17,6 +16,26 @@ export const DemoGrid = () => {
 
   const startY = Math.floor((-stagePos.y - window.innerHeight) / HEIGHT) * HEIGHT
   const endY = Math.floor((-stagePos.y + window.innerHeight * 2) / HEIGHT) * HEIGHT
+  const [scale, setScale] = React.useState(1.0)
+  const [windowSize, setWindowSize] = React.useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   const gridComponents = []
   let i = 0
@@ -31,28 +50,38 @@ export const DemoGrid = () => {
 
       gridComponents.push(
         <Rect
+          key={`${x / WIDTH}-${y / HEIGHT}`}
           x={x}
           y={y}
           width={WIDTH}
           height={HEIGHT}
           fill={grid[indexX][indexY]}
           stroke="black"
+          onClick={() => {
+            console.log(x / WIDTH, y / HEIGHT)
+          }}
         />
       )
     }
   }
   return (
-    <Stage
-      x={stagePos.x}
-      y={stagePos.y}
-      width={window.innerWidth}
-      height={window.innerHeight}
-      draggable
-      onDragEnd={(e) => {
-        setStagePos(e.currentTarget.position())
-      }}
-    >
-      <Layer>{gridComponents}</Layer>
-    </Stage>
+    <>
+      <button onClick={() => setScale(scale * 1.2)}>拡大する</button>
+      <button onClick={() => setScale(scale / 1.2)}>縮小する</button>
+      <p>{scale}</p>
+      <Stage
+        x={stagePos.x}
+        y={stagePos.y}
+        width={windowSize.width}
+        height={windowSize.height}
+        scale={{ x: scale, y: scale }}
+        draggable
+        onDragEnd={(e) => {
+          setStagePos(e.currentTarget.position())
+        }}
+      >
+        <Layer>{gridComponents}</Layer>
+      </Stage>
+    </>
   )
 }
