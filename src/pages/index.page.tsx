4e1d9128734bcb6b6c0d21e4ec49d0e49d05877c
Yoshiki Higuchi <s1f102201469@iniad.org>
2023-03-16 16:41:51 +0900

    fix: DemoGridをdynamic importへ変更

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,13 @@
 import { Space } from '@mantine/core'
 import { useAtom } from 'jotai'
+import dynamic from 'next/dynamic'
 import { BasicHeader } from 'src/components/organisms/BasicHeader'
 import { DemoModal } from 'src/pages/@components/modal'
 import { userAtom } from '../atoms/user'
-import { DemoGrid } from './@components/DemoGrid'
+
+const DemoGrid = dynamic(() => import('./@components/DemoGrid').then((mod) => mod.DemoGrid), {
+  ssr: false,
+})
 
 const Home = () => {
   const [user] = useAtom(userAtom)
